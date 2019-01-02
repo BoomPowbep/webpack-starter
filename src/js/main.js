8fd57d2b44189775d0b358abcf828397c20bfed1
mdebalme <mdebalme@agencenetdesign.com>
2019-01-02 17:07:23 +0100

    Removed 'load' event listener

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,6 @@
 import App from './App';
 
 window.addEventListener('DOMContentLoaded', () => {
-    window.addEventListener('load', () => {
         let app = new App();
         app.init();
-    });
 });
